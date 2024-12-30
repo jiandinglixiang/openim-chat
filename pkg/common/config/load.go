a62b0d7ba9621af7eba88dc9b0be00fa1ac1f39c
李* <jiandinglixiang@qq.com>
2024-12-30 21:28:19 +0800

    配置: 更新服务发现和连接设置
    - 更新 etcd 和 zookeeper 连接地址为远程服务器
    - 优化配置加载，支持环境变量覆盖
    - 添加配置文件读取的调试日志
    - 改进配置加载过程中的错误处理
    
    此次更改增强了配置管理系统：
    1. 支持远程服务发现端点
    2. 添加更清晰的配置文件错误提示
    3. 启用环境变量覆盖功能

--- pkg/common/config/load.go
+++ pkg/common/config/load.go
@@ -1,16 +1,30 @@
 package config
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/mitchellh/mapstructure"
 	"github.com/openimsdk/tools/errs"
 	"github.com/spf13/viper"
-	"strings"
 )
 
 func LoadConfig(path string, envPrefix string, config any) error {
 	v := viper.New()
+	fmt.Println("读取文件：", path)
 	v.SetConfigFile(path)
 	v.SetEnvPrefix(envPrefix)
+
+	// if err := v.ReadInConfig(); err != nil {
+	// 	return errs.WrapMsg(err, "failed to read config file", "path", path, "envPrefix", envPrefix)
+	// }
+
+	// // 打印从配置文件中读取到的变量
+	// fmt.Println("Configuration settings before AutomaticEnv:")
+	// for key, value := range v.AllSettings() {
+	// 	fmt.Printf("配置输出~%s: %v\n", key, value)
+	// }
+
 	v.AutomaticEnv()
 	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
